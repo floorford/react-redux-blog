9316a631a6c000b7ad5d046ff288e30df2f447f7
Flo Orford <floorford@Flos-MacBook-Pro.local>
2018-06-12 16:33:39 +0100

    initialising react-redux and fixing the blog to align with that

--- src/App.jsx
+++ src/App.jsx
@@ -8,11 +8,11 @@ import {
 import Header from "./components/Header";
 import FourOhFour from "./components/FourOhFour";
 
-import Articles from "./components/Articles/Articles";
-import Article from "./components/Articles/Article";
+import Articles from "./containers/Articles/Articles";
+import Article from "./containers/Articles/Article";
 import Add from "./components/Articles/Add";
 
-const App = ({ articles }) => (
+const App = () => (
     <React.Fragment>
         { /* header should show on all pages */ }
         <Header subtitle="Very insightful">My Blog</Header>
@@ -20,16 +20,15 @@ const App = ({ articles }) => (
         { /* route switch, to handle 404s */ }
         <Switch>
             { /* the articles list */ }
-            { /* use Object.values() to pass in an array of articles that we can map over */ }
-            <Route exact path="/" render={ () => <Articles articles={ Object.values(articles) } /> } />
+            <Route exact path="/" component={ Articles } />
 
             { /* show the add form - has to come before :id so "add" doesn't get treated as an id  */ }
             <Route exact path="/articles/add" component={ Add } />
 
-            { /* show an individual article */ }
+            { /* pass through the id to the container */ }
             <Route exact path="/articles/:id" render={ ({ match }) => (
-                <Article article={ articles[+match.params.id] } />
-            )} />
+              <Article id={ +match.params.id } /> )}
+            />
 
             { /* 404 page */}
             <Route component={ FourOhFour } />
--- src/index.js
+++ src/index.js
@@ -5,38 +5,28 @@ import ReactDOM from "react-dom";
 import history from "./history";
 import { Router } from "react-router-dom";
 
+// redux and react-redux relevant imports
+import { createStore } from "redux";
+import { Provider } from "react-redux";
+import reducer from "./data/reducer";
+import initial from "./data/initial";
+
 // import the App component
 import App from "./App";
 
-// create some dummy articles
-// store them in an object
-// so that we can easily reference each one by its id
-const articles = {
-    1: {
-        id: 1,
-        title: "Post #1",
-        article: "<p>Blah blah blah</p>",
-        comments: [
-            { email: "bob@bob.com", comment: "Great blog post" },
-        ],
-        tags: ["blah", "monkeys"],
-    },
-    2: {
-        id: 2,
-        title: "Post #2",
-        article: "<p>Blah blah blah</p>",
-        comments: [
-            { email: "sandi@sandi.com", comment: "I disagree, but not in an agressive and unpleasant manner" },
-        ],
-        tags: ["blah", "fish-fingers"],
-    },
-};
+const store = createStore(
+    reducer,
+    initial,
+    window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__(),
+);
 
 // wrap the app in Router
 // pass our articles through to App using props
 ReactDOM.render(
-    <Router history={ history }>
-        <App articles={ articles } />
-    </Router>,
+    <Provider store={ store }>
+        <Router history={ history }>
+            <App />
+        </Router>
+    </Provider>,
     document.getElementById("root"),
 );
