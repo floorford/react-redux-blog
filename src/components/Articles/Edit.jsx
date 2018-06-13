59cff0f728cbd68bb757ef973d28b284984008fc
Flo Orford <floorford@Flos-MacBook-Pro.local>
2018-06-13 09:13:28 +0100

    fixed the mapstatetoprops

--- src/components/Articles/Edit.jsx
+++ src/components/Articles/Edit.jsx
@@ -5,9 +5,9 @@ import FourOhFour from "../FourOhFour";
 
 
 // the add article component
-const Edit = ({ fields, id, article, title }) => !fields ? <FourOhFour /> : (
+const Edit = ({ fields, title }) => !fields ? <FourOhFour /> : (
     <React.Fragment>
-      <h2>Edit existing post: </h2>
+      <h2>Edit: { title }</h2>
 
       <Form className="panel-body" fields={ fields } button="Update Article" />
     </React.Fragment>
--- src/containers/Articles/Edit.js
+++ src/containers/Articles/Edit.js
@@ -3,15 +3,13 @@ import Edit from "../../components/Articles/Edit";
 
 // the second argument passed to mapStateToProps represent the props passed in from the parent
 const mapStateToProps = (state, { id }) => {
+  console.log(state)
   return {
-      // convert the articles object into an array
-      id: state.articles.id,
-      article: state.articles.article,
-      title: state.articles.title,
-      fields: [
-          { name: "title", label: "Title", value: state.articles[id].title },
-          { name: "article", label: "Article", value: state.articles[id].article },
-      ]
+    title: state.articles[id].title,
+    fields: [
+        { name: "title", label: "Title", value: state.articles[id].title },
+        { name: "article", label: "Article", value: state.articles[id].article },
+    ]
   };
 };
 
